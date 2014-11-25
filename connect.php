0ceeb6823730322741cfdfdd4aeab62915a931b2
TCC <john987john987@gmail.com>
2014-11-25 17:54:16 +0800

    Revert "第一次匯入檔案"
    This reverts commit 6a988e31bcf12d1a421edba77eb83e2d70c1c606.

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?
-	$dbhost = '127.0.0.1';
-	$dbuser = 'root';
-	$dbpass = 'csie110210';
-	$dbname = 'file-manager';
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
-	mysql_query("SET NAMES 'utf8'");
-	mysql_select_db($dbname);
-?>
